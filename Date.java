539d3c01fced1763189b0bad106202a2e1e0bae0
cmgabato <gabato.chaelle@gmail.com>
2016-09-20 15:59:37 +0800

    Update Date.java

--- Date.java
+++ Date.java
@@ -21,19 +21,19 @@ public class Date{
 
 	public static void main (String[] args) throws IOException{
 		Date d1 = new Date(2016, 2, 31);
-      	System.out.println(d1);  // toString()
+      		System.out.println(d1);  // toString()
  
-      // Test Setters and Getters
-     	d1.setYear(2012);
-     	d1.setMonth(12);
-     	d1.setDay(23);
-     	System.out.println(d1);  // run toString() to inspect the modified instance
-     	System.out.println("Year is: " + d1.getYear());
-     	System.out.println("Month is: " + d1.getMonth());
-     	System.out.println("Day is: " + d1.getDay());
- 	   	   // Test setDate()
-   	   d1.setDate(2988, 1, 2);
-   	   System.out.println(d1);  // toString()
+	      	// Test Setters and Getters
+	     	d1.setYear(2012);
+	     	d1.setMonth(12);
+	     	d1.setDay(23);
+	     	System.out.println(d1);  // run toString() to inspect the modified instance
+	     	System.out.println("Year is: " + d1.getYear());
+	     	System.out.println("Month is: " + d1.getMonth());
+	     	System.out.println("Day is: " + d1.getDay());
+	 	// Test setDate()
+   	   	d1.setDate(2988, 1, 2);
+   	   	System.out.println(d1);  // toString()
 	}
 
 	public int getYear(){
